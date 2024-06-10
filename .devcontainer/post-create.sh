8a3684a582e8e4f30c4daa3b280ad26ae41444b7
Stan Ht. Wu <stanwu.tw@gmail.com>
2024-06-10 04:37:00 +0000

    Debug

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-pwd
-#Init build environment
-sudo apt -y update && sudo apt -y install build-essential libncurses-dev bison flex libssl-dev libelf-dev fakeroot dwarves
-echo "Build environment installed"
+docker pull vaultwarden/server && docker run -d --name vaultwarden -v /data:/data -p 80:80 vaultwarden/server
+
