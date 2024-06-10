813721d8c4f3b0728efc3d0c0f7d8d7b8770053a
Stan Ht. Wu <stanwu.tw@gmail.com>
2024-06-10 05:35:42 +0000

    Update code

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,3 +1,1 @@
 #!/bin/bash
-
-/usr/bin/docker run -d --name vaultwarden -v /data:/data -p 80:80 vaultwarden/server
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker pull vaultwarden/server && sudo docker run -d --name vaultwarden -v /data:/data -p 80:80 vaultwarden/server
+docker pull vaultwarden/server && docker run -d --name vaultwarden -v /data:/data -p 80:80 vaultwarden/server
